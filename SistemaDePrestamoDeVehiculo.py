f055fa8a65ed2671fbac84fed7cadb8eb1ec199f
Gabriel_Garcia <gabrielalejandrogarciarivas0@gmail.com>
2022-08-27 15:48:35 -0400

    Se establecio una conexion con la base de datos

--- SistemaDePrestamoDeVehiculo.py
+++ SistemaDePrestamoDeVehiculo.py
@@ -19,3 +19,28 @@ class create:
             print('base de datos creada exitosamente')
 
         self.conexion.close()
+
+    def vehiculo(self):
+        self.conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="123456",
+            database="Seguro"
+        )
+        self.cursor = self.conexion.cursor()
+        try:
+            self.cursor.execute('''
+                    CREATE TABLE vehiculo (
+                        nombreDueño VARCHAR(100) NOT NULL,
+                        matricula VARCHAR(100) UNIQUE NOT NULL PRIMARY KEY,
+                        año VARCHAR(100) NOT NULL,
+                        marca VARCHAR(100) NOT NULL,
+                        Color VARCHAR(100),
+                        pago INTEGER NOT NULL
+                    )
+                ''')
+        except mysql.connector.errors.ProgrammingError:
+            print('bienvenido a la tabla vehiculo')
+        else:
+            print('tabla vehiculo creada correctamente')
+        self.conexion.close()
