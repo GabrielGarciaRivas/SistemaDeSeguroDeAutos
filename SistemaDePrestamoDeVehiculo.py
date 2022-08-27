84f3e624d76ca4db5c5db4820a5cd1d318b82828
EdwardMontero <monteroedward23401@gmail.com>
2022-08-27 15:45:58 -0400

    Se anadieron algunas clases

--- SistemaDePrestamoDeVehiculo.py
+++ SistemaDePrestamoDeVehiculo.py
@@ -1,1 +1,21 @@
 import mysql.connector
+
+class create:
+    def _init_(self):
+        self.crear_bd()
+        self.vehiculo()
+    def crear_bd(self):
+        self.conexion = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="123456"
+        )
+        self.cursor = self.conexion.cursor()
+        try:
+            self.cursor.execute('CREATE DATABASE nombre de la base de datos')
+        except mysql.connector.errors.DatabaseError:
+            print('bienvenido a la base de datos -nombre de la base de datos-')
+        else:
+            print('base de datos creada exitosamente')
+
+        self.conexion.close()
